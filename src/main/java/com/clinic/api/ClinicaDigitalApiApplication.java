893fc9da93539f0c9cb154dffc23ab908d7eca6f
Sara Lyma <saralyma2022@gmail.com>
2026-01-09 17:45:26 -0300

    Primeiro commit: Estrutura inicial do projeto

---
+++ src/main/java/com/clinic/api/ClinicaDigitalApiApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaDigitalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaDigitalApiApplication.class, args);
+    }
+
+}
